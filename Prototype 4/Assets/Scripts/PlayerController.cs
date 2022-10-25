ec0ecd42cfda9c2f69e3458ed1fba1412e8eedb2
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-10-25 11:00:38 -0700

    Commit main (cyberpunk reference) why is he gone

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    public float Speed = 10;
+
+    private Rigidbody2D _playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+        _playerRB.AddForce(direction * Speed);
+    }
+}
