fc9f9ed717a065d9ba3fe1c79d88ace5ce0eef50
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-11-03 11:03:57 -0700

    things

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class PlayerController : MonoBehaviour
-{   
+{
     public float Speed = 10;
-    
-    private Rigidbody2D _playerRB;
     public GameObject ExplosionFX;
+    public GameObject PowerupIndicator;
+    public bool HasPowerup = false;
+    private Rigidbody2D _playerRb;
+    public float PowerupStrength = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        _playerRB = GetComponent<Rigidbody2D>();
+        _playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,18 +24,46 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
 
         Vector2 direction = new Vector2(horizontalInput, verticalInput);
-        _playerRB.AddForce(direction * Speed);
+        _playerRb.AddForce(direction * Speed);
+
     }
+
     private void OnTriggerEnter2D(Collider2D other)
-     {
-      if(other.gameObject.CompareTag("Wall"))
-      {
-        Instantiate(ExplosionFX, transform.position, ExplosionFX.transform.rotation);
-        gameObject.SetActive(false);
-        SceneManager.LoadScene(0);
-      }  
+    {
+        if(other.gameObject.CompareTag("Wall"))
+        {
+            Instantiate(ExplosionFX, transform.position, ExplosionFX.transform.rotation);
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(0);
+        }
 
-
+        if(other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            PowerupIndicator.gameObject.SetActive(true);
+            HasPowerup = true;
+            StartCoroutine(PowerupCountdownRoutine());
+        }
         
     }
+    private void OnCollision2D (Collision2D other)
+        {
+            if(other.gameObject.CompareTag("Enemy") && HasPowerup)
+            {
+                Rigidbody2D enemyRB = other.gameObject.GetComponent<Rigidbody2D>();
+
+                Vector2 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+
+                enemyRB.AddForce(awayFromPlayer * PowerupStrength, ForceMode2D.Impulse);
+
+                PowerupIndicator.gameObject.SetActive(false);
+                HasPowerup = false;
+            }
+        }
+        IEnumerator PowerupCountdownRoutine()
+        {
+            yield return new WaitForSeconds(5);
+            PowerupIndicator.gameObject.SetActive(false);
+            HasPowerup = false;
+        }
 }
--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject EnemyPrefab;
-    public float XRange = 6.4f;
-    public float YRange = 2.66f;
+    public GameObject PowerupPrefab;
+    public float XRange = 5.5f;
+    public float YRange = 2.5f;
     public int EnemyCount = 0;
     public int WaveCount = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-       
         SpawnEnemyWave(WaveCount);
-        //Instantiate(EnemyPrefab, GenerateSpawnPostition(), EnemyPrefab.transform.rotation);
+        Instantiate(PowerupPrefab, GenerateSpawnPosition(), PowerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -23,27 +23,28 @@ public class SpawnManager : MonoBehaviour
     {
         EnemyCount = FindObjectsOfType<EnemyController>().Length;
 
-        if(EnemyCount ==0)
+        if(EnemyCount == 0)
         {
-            WaveCount++;
-             SpawnEnemyWave(WaveCount);
+            WaveCount++; // WaveCount = WaveCount + 1;
+            SpawnEnemyWave(WaveCount);
+            Instantiate(PowerupPrefab, GenerateSpawnPosition(), PowerupPrefab.transform.rotation);
         }
     }
 
-    private Vector2 GenerateSpawnPostition()
+    private Vector2 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-XRange, XRange);
         float spawnPosY = Random.Range(-YRange, YRange);
         Vector2 randomPos = new Vector2(spawnPosX, spawnPosY);
-        return  randomPos;
+        return randomPos;
     }
+
     private void SpawnEnemyWave(int enemiesToSpawn)
     {
         for(int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(EnemyPrefab, GenerateSpawnPostition(), EnemyPrefab.transform.rotation);
+            Instantiate(EnemyPrefab, GenerateSpawnPosition(), EnemyPrefab.transform.rotation);
         }
         
     }
-
 }
